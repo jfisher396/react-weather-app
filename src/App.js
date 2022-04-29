1042292850c513975c7bd32cf6b50ea4be01812a
James Fisher <jfisher396@hotmail.com>
2022-04-28 21:02:19 -0500

    API connected and returning data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Card from "./Components/Card";
+import axios from "axios";
 
 function App() {
+  // coordinates for Houston, TX
+  const [locationData] = useState({
+    latitude: "29.7589382",
+    longitude: "-95.3676974",
+  });
+
+  useEffect(() => {
+    try {
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${locationData.latitude}&lon=${locationData.longitude}&appid=42d98d76405f5b8038f2ad71187af430`
+        )
+        .then((res) => {
+          console.log(res.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="container">
+          <Card />
+        </div>
+      </div>
+    </>
   );
 }
 
